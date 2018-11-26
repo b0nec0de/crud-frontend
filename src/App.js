36eceddfe667b6827ae1157aeb1cfe23d9c586ce
Anton Bondarev <a.bondarev@wellyes.ru>
2018-11-26 19:09:08 +0300

    auth form simple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/api/url', {
+      method: 'POST',
+      body: this.state,
+    });
+
+    console.log(this.state);
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="auth">
+        <div className="auth__wrapper">
+          <form className="auth__form" onSubmit={this.handleSubmit}>
+            <label>
+              E-mail
+              <input
+                type="email"
+                name="email"
+                onChange={this.handleInputChange} />
+            </label>
+            <label>
+              Password
+              <input
+                type="password"
+                name="password"
+                onChange={this.handleInputChange} />
+            </label>
+            <button className="btn auth__btn" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
