5786c1ee93d3f4217c259d0796295cbc8c72a6b7
Anton Bondarev <a.bondarev@wellyes.ru>
2018-11-30 19:13:15 +0300

    before weekend

--- src/App.js
+++ src/App.js
@@ -1,87 +1,20 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { withCookies } from 'react-cookie';
+import React from 'react';
+import { CookiesProvider } from 'react-cookie';
 import './App.css';
 
-
-class App extends Component {
-	constructor(props) {
-		super(props);
-		const { cookies } = this.props;
-		this.state = {
-			email: '',
-			password: '',
-			token: cookies.get('token')
-		};
-	}
-
-	handleInputChange = event => {
-		this.setState({
-			[event.target.name]: event.target.value
-		});
-	};
-
-	handleSubmit = event => {
-		event.preventDefault();
-		const { email, password, token } = this.state;
-		const { cookies } = this.props;
-		// axios.post(
-		// 	'http://localhost:3001/auth', {
-		// 		config: {
-		// 			headers: {
-		// 				'content-type': 'application/x-www-form-urlencoded'
-		// 			},
-		// 			data: {
-		// 				email,
-		// 				password
-		// 			}
-		// 		}
-		// 	}
-		// )
-
-		axios.post(
-			'http://localhost:3001/auth',
-			{ email, password }
-		)
-			.then(response => {
-				cookies.set('token', response.data.token)
-			})
-			.then(this.setState({ token }))
-			.catch(err => console.log(err))
-	}
+import Auth from './components/Auth/Auth';
 
 
-	render() {
-		return (
-			<div className="auth" >
-				<div className="auth__wrapper">
-					<form className="auth__form" onSubmit={this.handleSubmit}>
-						<label className="auth__label">
-							<span className="auth__label-text">email</span>
-							<input
-								className="auth__input"
-								type="email"
-								name="email"
-								onChange={this.handleInputChange}
-							/>
-						</label>
-						<label className="auth__label">
-							<span className="auth__label-text">password</span>
-							<input
-								className="auth__input"
-								type="password"
-								name="password"
-								onChange={this.handleInputChange}
-							/>
-						</label>
-						<button className="btn auth__btn" type="submit">
-							submit
-						</button>
-					</form>
-				</div>
-			</div>
-		);
-	}
+function App(props) {
+
+	return (
+		<div className="app" >
+			<CookiesProvider>
+				<Auth />
+			</CookiesProvider>
+		</div>
+	);
+
 }
 
-export default withCookies(App);
+export default App;
